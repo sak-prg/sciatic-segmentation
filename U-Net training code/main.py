d466689c0af1c59cf2a9ad46efffa5a0e9421b19
sak-prg <92521255+sak-prg@users.noreply.github.com>
2022-04-01 15:43:52 +0500

    Add files via upload

---
+++ U-Net training code/main.py
@@ -0,0 +1,131 @@
+import os
+import matplotlib.pyplot as plt
+import numpy
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow import keras
+import keras.backend as K
+
+# Define constants
+SEED = 909
+BATCH_SIZE_TRAIN = 32
+BATCH_SIZE_TEST = 32
+
+IMAGE_HEIGHT = 512
+IMAGE_WIDTH = 512
+IMG_SIZE = (IMAGE_HEIGHT, IMAGE_WIDTH)
+
+data_dir = 'D:\proj\mask'
+data_dir_train = os.path.join(data_dir, 'training')
+# The images should be stored under: "data/slices/training/img/img"
+data_dir_train_image = os.path.join(data_dir_train, 'img')
+# The images should be stored under: "data/slices/training/mask/img"
+data_dir_train_mask = os.path.join(data_dir_train, 'mask')
+
+data_dir_test = os.path.join(data_dir, 'test')
+# The images should be stored under: "data/slices/test/img/img"
+data_dir_test_image = os.path.join(data_dir_test, 'img')
+# The images should be stored under: "data/slices/test/mask/img"
+data_dir_test_mask = os.path.join(data_dir_test, 'mask')
+
+NUM_TRAIN = 160
+NUM_TEST = 160
+
+NUM_OF_EPOCHS = 1
+
+
+def create_segmentation_generator_train(img_path, msk_path, BATCH_SIZE):
+    data_gen_args = dict(rescale=1. / 255
+                         #                      featurewise_center=True,
+                         #                      featurewise_std_normalization=True,
+                         #                      rotation_range=90
+                         #                      width_shift_range=0.2,
+                         #                      height_shift_range=0.2,
+                         #                      zoom_range=0.3
+                         )
+    datagen = ImageDataGenerator(**data_gen_args)
+
+    img_generator = datagen.flow_from_directory(img_path, target_size=IMG_SIZE, class_mode=None, color_mode='grayscale',
+                                                batch_size=BATCH_SIZE, seed=SEED)
+    msk_generator = datagen.flow_from_directory(msk_path, target_size=IMG_SIZE, class_mode=None, color_mode='grayscale',
+                                                batch_size=BATCH_SIZE, seed=SEED)
+    return zip(img_generator, msk_generator)
+
+
+# Remember not to perform any image augmentation in the test generator!
+def create_segmentation_generator_test(img_path, msk_path, BATCH_SIZE):
+    data_gen_args = dict(rescale=1. / 255)
+    datagen = ImageDataGenerator(**data_gen_args)
+
+    img_generator = datagen.flow_from_directory(img_path, target_size=IMG_SIZE, class_mode=None, color_mode='grayscale',
+                                                batch_size=BATCH_SIZE, seed=SEED)
+    msk_generator = datagen.flow_from_directory(msk_path, target_size=IMG_SIZE, class_mode=None, color_mode='grayscale',
+                                                batch_size=BATCH_SIZE, seed=SEED)
+    return zip(img_generator, msk_generator)
+
+train_generator = create_segmentation_generator_train(data_dir_train_image, data_dir_train_mask, BATCH_SIZE_TRAIN)
+test_generator = create_segmentation_generator_test(data_dir_test_image, data_dir_test_mask, BATCH_SIZE_TEST)
+
+
+def display(display_list):
+    plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]), cmap='gray')
+    plt.show()
+
+
+def show_dataset(datagen, num=1):
+    for i in range(0,num):
+        image,mask = next(datagen)
+        display([image[0], mask[0]])
+
+show_dataset(train_generator, 2)
+
+
+def unet(n_levels, initial_features=32, n_blocks=2, kernel_size=3, pooling_size=2, in_channels=1, out_channels=1):
+    inputs = keras.layers.Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, in_channels))
+    x = inputs
+
+    convpars = dict(kernel_size=kernel_size, activation='relu', padding='same')
+
+    # downstream
+    skips = {}
+    for level in range(n_levels):
+        for _ in range(n_blocks):
+            x = keras.layers.Conv2D(initial_features * 2 ** level, **convpars)(x)
+        if level < n_levels - 1:
+            skips[level] = x
+            x = keras.layers.MaxPool2D(pooling_size)(x)
+
+    # upstream
+    for level in reversed(range(n_levels - 1)):
+        x = keras.layers.Conv2DTranspose(initial_features * 2 ** level, strides=pooling_size, **convpars)(x)
+        x = keras.layers.Concatenate()([x, skips[level]])
+        for _ in range(n_blocks):
+            x = keras.layers.Conv2D(initial_features * 2 ** level, **convpars)(x)
+
+    # output
+    activation = 'sigmoid' if out_channels == 1 else 'softmax'
+    x = keras.layers.Conv2D(out_channels, kernel_size=1, activation=activation, padding='same')(x)
+
+    return keras.Model(inputs=[inputs], outputs=[x], name=f'UNET-L{n_levels}-F{initial_features}')
+
+
+EPOCH_STEP_TRAIN = NUM_TRAIN // BATCH_SIZE_TRAIN
+EPOCH_STEP_TEST = NUM_TEST // BATCH_SIZE_TEST
+
+model = unet(4)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+model.fit_generator(generator=train_generator,
+                    steps_per_epoch=EPOCH_STEP_TRAIN,
+                    validation_data=test_generator,
+                    validation_steps=EPOCH_STEP_TEST,
+                   epochs=NUM_OF_EPOCHS)
